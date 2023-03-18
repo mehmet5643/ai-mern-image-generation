ec467591608b41e42c0627fe7ac4b95327207cf5
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-03-19 01:36:41 +0300

    changes updated

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({labelName,type,name,placeholder,value,handleChange,isSurpriseMe,handleSurpriseMe}) => {
   return (
-    <div>FormField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label className='block font-medium text-gray-900 text-sm' htmlFor={name}>{labelName}</label>
+        {isSurpriseMe && (
+          <button className='font-semibold text-xs bg-[#ecef1] py-1 px-2 rounded-[5px] text-black' onClick={handleSurpriseMe}>Surprise me</button>
+        )}
+
+
+      </div>
+      <input 
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange=  {handleChange}
+        required
+        className='w-full border border-gray-200 bg-gray-50  text-gray-900  py-2 text-sm rounded-lg block p-3  outline-none focus:ring-[#4649ff] focus:border-transparent'
+
+      >
+      
+      </input>
+    </div>
   )
 }
 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,9 +13,73 @@ const CreatePost = () => {
     photo: "",
   });
   const [genaratingImg, setGeneratingImg] = useState(false);
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
 
   const navigate = useNavigate();
-  return <section className="max-w-7xl mx-auto"></section>;
+  return <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className='font-extrabold text-[#222128] text-[32px]'>
+          Create Post
+          <p className='mt-2 text-[#666e75] text-[16px] max-w [500px]'>
+          Create imaginative and visually stunning images through DALL-E AI and share them with the community
+          </p>
+        </h1> 
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Enter your name (example: Eflatun)"
+            value={form.name}
+            handleChange= {handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="Enter your prompt (example: A cat wearing a hat)"
+            value={form.prompt}
+            handleChange= {handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+  </section>;
 };
 
 export default CreatePost;
