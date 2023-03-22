7809bdcd1773de5c7c44fbc16cfcd9c0b6597ffc
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-03-22 23:37:39 +0300

    post routes updated

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,28 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+// GET ALL POSTS
+router.get("/", async (req, res) => {
+
+})
+
+// CREATE A POST
+router.post("/", async (req, res) => {
+    const {name,prompt ,photo} = req.body;
+    const result = await cloudinary.uploader.upload(photo)
+
+    const newPost = new Post({
+        name,
+        prompt,
+        photo: result.secure_url
+    })
+
+})
+
+
 export default router;
