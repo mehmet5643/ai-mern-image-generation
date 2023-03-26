11c3d15b7908ec88443590e15cc9142b7c1d8f8f
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-03-27 01:59:37 +0300

    bek ai updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
       <Link to="/">
-      <img src={logo} alt="logo" className='w-28 object-contain'/>
+      <img src={logo} alt="logo" className='w-40 object-contain'/>
       </Link>
       <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white rounded-md px-4 py-2'>
         Create Post
